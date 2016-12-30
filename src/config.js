fe4c1f65a3617233f242a482854b9f4dd64e4af3
Hammster <mail@hans-koch.me>
2016-12-30 10:38:18 +0100

    Remove unused code

--- src/config.js
+++ src/config.js
@@ -20,19 +20,5 @@ config.server = {
     }
 };
 
-//config.server = [1,2,3];
-/*
-config.server['teamspeak3'] = {};
-config.server['teamspeak3'].host = "das-uhrwerk.de";
-config.server['teamspeak3'].port = 7777;
-
-config.server['starbound'] = {};
-config.server['starbound'].host = "das-uhrwerk.de";
-config.server['starbound'].port = 21025;
-
-config.server['arkse'] = {};
-config.server['arkse'].host = "das-uhrwerk.de";
-config.server['arkse'].port = 8888;
-*/
 // export
 module.exports = config;
